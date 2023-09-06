0827252e7ea5b604f4d0dd3b830d022142bb0113
Ajay kaarthic J <33996820+ajaykaarthic@users.noreply.github.com>
2023-09-06 15:22:43 -0500

    Update filedownload.py

--- filedownload.py
+++ filedownload.py
@@ -10,15 +10,17 @@ soup = BeautifulSoup(html, 'html.parser')
 anchor_tags = soup.find_all('a')
 
 # Download the links
+count = 1
 for tag in anchor_tags:
     link = tag.get('href')
     if link.startswith('/'):  # Handle relative URLs
-        link = 'http://192.168.153.212:5678' + link
+        link = 'http://192.168.43.1:5678' + link
     response = requests.get(link)
     if response.status_code == 200:
         filename = link.split('/')[-1]  # Extract the filename from the URL
         with open(filename, 'wb') as f:
             f.write(response.content)
-        print(f"Downloaded: {filename}")
+        print(f"{count} file downloaded: {filename}")
+        count += 1
     else:
         print(f"Failed to download: {link}")
